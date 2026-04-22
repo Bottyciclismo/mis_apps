bd7ee6232e857a3b53d7f1d4e7e45d464b5968f7
botty <juancbotella@gmail.com>
2026-04-22 20:14:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
+const CACHE_NAME = "botty-app-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/img/fondomio.png",
+  "/manifest.json"
+];
+
+// INSTALAR
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ACTIVAR
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// FETCH (offline)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
